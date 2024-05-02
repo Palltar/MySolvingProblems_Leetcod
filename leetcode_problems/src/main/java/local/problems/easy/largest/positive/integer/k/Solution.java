f2a4e691998b1c31f7993bdd57f890a1188420eb
Pavel B <begunkovich.pavel@yandex.ru>
2024-05-03 00:13:22 +0300

    first solution to problem 2441

---
+++ leetcode_problems/src/main/java/local/problems/easy/largest/positive/integer/k/Solution.java
@@ -0,0 +1,24 @@
+package local.problems.easy.largest.positive.integer.k;
+
+
+import java.util.Arrays;
+
+//Given an integer array nums that does not contain any zeros,
+//find the largest positive integer k such that -k also exists in the array.
+//Return the positive integer k. If there is no such integer, return -1.
+
+public class Solution {
+    public int findMaxK(int[] nums) {
+        int res = -1;
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length - 1;
+        while ( i < j ){
+            if (-nums[i] == nums[j]) return nums[j];
+            else if (-nums[i] > nums[j]) i++;
+            else j--;
+        }
+        return -1;
+    }
+}
+
